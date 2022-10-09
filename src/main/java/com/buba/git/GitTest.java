48256899c62c570cb493f2fbf685781edbde9574
xinliandewang <xinlian@qq.com>
2022-10-09 16:05:30 +0800

    这是第一个Java

--- src/main/java/com/buba/git/GitTest.java
+++ src/main/java/com/buba/git/GitTest.java
@@ -2,6 +2,6 @@ package com.buba.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello kugou");
     }
 }
